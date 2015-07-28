22c1c5208109fa54369806615c0adaf39df1875c
Ramun McCallum <matthew@synapseware.com>
2015-07-27 22:25:32 -0700

    SMA complete.  EMA stub in place.

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ filters
 A demo repo for implementing practice versions of various low-pass filters such as Simple Moving Average (SMA), Exponential Moving Average (EMA) and possibly more.
 
 The main objective is to find a very simple implementation of an EMA that can work on a variable dataset (2^N samples) where N can be 1 to 4 or 5.  It will also explore how the size of a dataset affects the responsiveness of the input sample for different scenarios.  For example, when sampling voltage from a current sense resistor in a power supply, what's the ideal data set to work with?  Or, when sampling an accelerometer or temperature sensor?
+
+
+### Sources
+http://bleaklow.com/2012/06/20/sensor_smoothing_and_optimised_maths_on_the_arduino.html
+
+http://electronics.stackexchange.com/questions/42734/fastest-moving-averaging-techniques-with-minimum-footprint
+
--- filter.cpp
+++ filter.cpp
@@ -5,6 +5,16 @@ using namespace std;
 
 const int dataLen = 32;
 static int data[dataLen];
+static int writePos = 0;
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - -
+void push_sample(int sample)
+{
+	data[writePos++] = sample;
+	if (writePos > dataLen - 1)
+		writePos = 0;
+}
 
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - -
@@ -22,6 +32,30 @@ int getFileSize(FILE* file)
 
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Computes a simple moving average
+int filter_sma(void)
+{
+	long average = 0;
+	for (int i = 0; i < dataLen; i++)
+	{
+		average += data[i];
+	}
+
+	average = average / dataLen;
+
+	return average;
+}
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// 
+int filter_ema(void)
+{
+	return -1;
+}
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 int main(int argc, char* argv[])
 {
 	if (argc < 6)
@@ -30,14 +64,13 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 
-
 	char *type = NULL,
 		*inputFile = NULL,
 		*outputFile = NULL;
 
 	for (int i = 0; i < argc; i++)
 	{
-		if (0 == strncmp("-f", argv[i], 2))
+		if (0 == strncmp("-t", argv[i], 2))
 			type = argv[++i];
 		else if (0 == strncmp("-i", argv[i], 2))
 			inputFile = argv[++i];
@@ -66,6 +99,29 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
+	cout << "Processing input data " << inputFile << " using the " << type << " filter." << endl;
+	cout << "Results will be saved to " << outputFile << endl;
+	cout << endl;
+
+	for (int i = 0; i < 256; i+= 2)
+	{
+		push_sample(i);
+
+		int sample = 0;
+		if (0 == strncmp("sma", type, 3))
+		{
+			sample = filter_sma();
+		}
+		else if (0 == strncmp("ema", type, 3))
+		{
+			sample = filter_ema();
+		}
+
+		cout << "Average: " << sample << endl;
+	}
+
+	cout << endl;
+
 	return 0;
 }
 
@@ -76,8 +132,8 @@ void showHelp(void)
 	cout << "  SMA: Simple Moving Average" << endl;
 	cout << "  EMA: Exponential Moving Average" << endl;
 	cout << endl;
-	cout << "filter -f {type} -i {input file} -o {output file}" << endl;
-	cout << "  -f  Filter type.  Can be either sma or ema." << endl;
+	cout << "filter -t {type} -i {input file} -o {output file}" << endl;
+	cout << "  -t  Filter type.  Can be either sma or ema." << endl;
 	cout << "  -i  Path to input file which is a text file.  One data point per line." << endl;
 	cout << "  -o  Path to output file.  File will be written as a series of data points, one per line.  It may not have the " \
 	" same number of lines as the input file." << endl;
